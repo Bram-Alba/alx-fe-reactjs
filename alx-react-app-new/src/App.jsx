fe988524007a4f49393040335181cd21eeac36d1
Bram-Alba <abrahamabaya002@gmail.com>
2026-01-16 23:29:22 +0100

    Add Counter component and update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,11 +2,14 @@ import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
     <>
       <Header />
+      import Counter from './components/Counter';
       
       {/* At least one UserProfile immediately after header */}
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
