394f5820f951c35c92619d6f5bce3844f75d07db
Bram-Alba <abrahamabaya002@gmail.com>
2026-01-17 20:42:12 +0100

    Fix Context API implementation for ALX checks

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import UserProfile from "./components/UserProfile";
 import UserContext from "./UserContext";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com",
+  };
 
   return (
     <UserContext.Provider value={userData}>
