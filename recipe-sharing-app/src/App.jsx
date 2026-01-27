94493e2ca5292832312aa7dad82ae03df756d950
Bram-Alba <Abrahamabaya002@gmail.com>
2026-01-27 12:33:45 +0100

    Move recipeStore.js to components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,17 @@
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
-import './App.css';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Recipe Sharing App</h1>
+    <BrowserRouter>
       <AddRecipeForm />
-      <RecipeList />
-    </div>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
