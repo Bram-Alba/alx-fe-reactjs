77c032765f8b599a0b544f379f8865a8f9cbdb29
Bram-Alba <Abrahamabaya002@gmail.com>
2026-01-29 16:46:46 +0100

    moved packagw.json inside src

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Search from './components/Search'
 
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Project setup complete!</p>
+      <Search />
     </div>
   )
 }
