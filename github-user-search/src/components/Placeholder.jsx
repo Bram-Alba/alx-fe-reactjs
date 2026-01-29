1e0b059b2c0bab498df885e556b0c2dda397e6a7
Bram-Alba <Abrahamabaya002@gmail.com>
2026-01-29 16:41:09 +0100

    github user search4

---
+++ github-user-search/src/components/Placeholder.jsx
@@ -0,0 +1,6 @@
+// src/components/Placeholder.jsx
+function Placeholder() {
+  return <div>Component folder ready!</div>;
+}
+
+export default Placeholder;
---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,6 @@
+// src/components/Search.jsx
+function Search() {
+  return <div>Search Component Ready</div>;
+}
+
+export default Search;
