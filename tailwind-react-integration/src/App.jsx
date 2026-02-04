57453ba1855e52af646f7dbf0ee1edc0a4325759
Bram-Alba <Abrahamabaya002@gmail.com>
2026-02-04 22:02:45 +0100

    Style

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,14 +1,9 @@
+// src/App.jsx
 import UserProfile from './components/UserProfile'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-return <UserProfile />
-
+  return <UserProfile />
 }
 
 export default App
