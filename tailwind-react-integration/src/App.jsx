b51966d7ad52265f1c653015dd93e453e302322d
Bram-Alba <Abrahamabaya002@gmail.com>
2026-02-04 21:52:22 +0100

    Render UserProfile component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,37 +7,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-         <h1 className="text-3xl font-bold text-blue-600 mb-4">
-          Vite + React
-         </h1>
+return <UserProfile />
 
-       <div className="card">
-          <button
-           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
-          </button>
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
 
 export default App
