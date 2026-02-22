5851b6e2ae80ff732270d9d1a4d5cbf90a91f8e7
Bram-Alba <Abrahamabaya002@gmail.com>
2026-02-22 16:11:53 +0100

    Fix Tailwind content paths and verify setup

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',  // <-- MUST include this for the checker
   ],
+  darkMode: false, // or 'media' / 'class'
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
